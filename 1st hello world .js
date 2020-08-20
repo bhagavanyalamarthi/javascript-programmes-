0bc073dcea4ff60f89e281fccacaa6c7c4ddba65
bhagavanyalamarthi <61960124+bhagavanyalamarthi@users.noreply.github.com>
2020-08-20 22:00:40 +0530

    Create 1st hello world .js

---
+++ 1st hello world .js
@@ -0,0 +1,27 @@
+Task
+
+A greeting function is provided for you in the editor below. It has one parameter, . parameterVaraiable Perform the following tasks to complete this challenge:
+
+Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+Use console.log() to print the contents of parameterVaraiable  (i.e., the argument passed to main).
+You've got this!
+
+code
+
+'function greeting(parameterVariable) {
+    console.log('Hello, World!');
+
+     console.log(parameterVariable);/////this is the answer
+    
+}
+
+
+function main() {
+    const parameterVariable = readLine();
+    
+    greeting(parameterVariable);
+}
+
+
+
+
